322bdc4b7c621c3bf66f0eb9748f6882a066f693
Ymiee <xx770930@gmail.com>
2021-09-05 23:26:26 +0800

    Update 2021-09-05T15:26:20.536Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,62 +1,63 @@
+"use strict";
+
 $(document).ready(function () {
   $('.toggleMenu').on('click', function (e) {
     e.preventDefault();
     $('body').toggleClass('showMenu');
   });
 });
-
-const classSwiper = new Swiper(".classSwiper", {
+var classSwiper = new Swiper(".classSwiper", {
   slidesPerView: 1,
   grid: {
-    rows: 3,
+    rows: 3
   },
   spaceBetween: 8,
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-prev"
   },
   breakpoints: {
     768: {
       slidesPerView: 2,
       grid: {
-        rows: 2,
+        rows: 2
       },
-      spaceBetween: 30,
+      spaceBetween: 30
     },
     1200: {
       slidesPerView: 3,
       grid: {
-        rows: 2,
+        rows: 2
       },
-      spaceBetween: 30,
+      spaceBetween: 30
     }
   }
 });
-
-const feebackSwiper = new Swiper(".feebackSwiper", {
+var feebackSwiper = new Swiper(".feebackSwiper", {
   slidesPerView: 1,
   grid: {
-    rows: 3,
+    rows: 3
   },
   spaceBetween: 8,
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-prev"
   },
   breakpoints: {
     768: {
       slidesPerView: 2,
       grid: {
-        rows: 2,
+        rows: 2
       },
-      spaceBetween: 30,
+      spaceBetween: 30
     },
     1200: {
       slidesPerView: 3,
       grid: {
-        rows: 2,
+        rows: 2
       },
-      spaceBetween: 30,
+      spaceBetween: 30
     }
   }
 });
+//# sourceMappingURL=all.js.map
